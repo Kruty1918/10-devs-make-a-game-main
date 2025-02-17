b6e4ac291a847e327803f2230272aeac72657685
Kruty1918 <131464148+Kruty1918@users.noreply.github.com>
2025-02-17 12:15:29 +0100

    Update MaterialItemChecker.cs

--- Assets/Project/Scripts/Utilities/MaterialItemChecker.cs
+++ Assets/Project/Scripts/Utilities/MaterialItemChecker.cs
@@ -1,20 +1,16 @@
 using Bonjoura.Player;
 using Bonjoura.Craft;
 using SGS29.Utilities;
+using Bonjoura.UI;
 
 namespace Bonjoura.Utilities
 {
-    public class MaterialItemChecker
+    public static class MaterialItemChecker
     {
-        private static MaterialItemChecker instance;
-        public static MaterialItemChecker Instance => instance ??= new MaterialItemChecker();
-
-        private MaterialItemChecker() { }
-
-        public bool IsExist(MaterialItem material)
+        public static bool IsExist(MaterialItem material)
         {
-            var inventory = SM.Instance<PlayerController>().ItemInventory;
-            var quantity = inventory.GetItemQuantity(material.item);
+            ItemInventory inventory = SM.Instance<PlayerController>().ItemInventory;
+            int quantity = inventory.GetItemQuantity(material.item);
 
             if (material.quantity > quantity)
                 return false;
