5caf5dc1bc91593b7f575003df2cad7c161f71eb
Kruty1918 <131464148+Kruty1918@users.noreply.github.com>
2025-02-17 13:38:42 +0100

    Update SlotData.cs

--- Assets/Project/Scripts/Controllers/UI/SlotData.cs
+++ Assets/Project/Scripts/Controllers/UI/SlotData.cs
@@ -1,7 +1,13 @@
 namespace Bonjoura.UI.Inventory
 {
+    /// <summary>
+    /// A class that describes an inventory slot 
+    /// </summary>
     public class SlotData
     {
+        /// <summary>
+        /// Whether the slot is occupied 
+        /// </summary>
         public bool IsOccupied { get; private set; }
 
         public SlotData(bool isOccupied)
